50227cf067a8a3a1cc196558e219941793734ab6
NurulHelda <nurul.helda.tif20@gmail.com>
2023-10-08 15:46:16 +0700

    change

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-var tombolMenu = document.getElementsByClassName('tombol-menu')[0];
-var menu = document.getElementsByClassName('menu')[0];
+function doCheckCost(){
+    const requestOngkir = new Request("https://eok8jd671o4psp5.m.pipedream.net",{
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        origin : document.getElementById("origin").value,
+        destination : document.getElementById("destination").value,
+        weight : document.getElementById("weight").value
+    })
+});
 
-tombolMenu.onclick = function() {
-    menu.classList.toggle('active');
+    const responseOngkir = fetch(requestOngkir);
+
+    responseOngkir
+    .then((reponse)=>reponse.json())
+    .then(function(json){
+        document.getElementById("ongkirResult").textContent = json.responseSucsess;
+    })
+    .catch(function(error){
+        document.getElementById("ongkirResult").textContent = error;
+    })
 }
 
-menu.onclick = function() {
-    menu.classList.toggle('active');
-}
-
-
-const inputNumberTrack = document.querySelector("[value]");
-inputNumberTrack.addEventListener("click", function() {
-    inputNumberTrack.textContent = " ";
-})
+document.getElementById("checkCost").onclick = doCheckCost;
